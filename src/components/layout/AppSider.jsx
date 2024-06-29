12325253db001849dd05fbb790d24253709e4af6
thumb22 <bogdankreminskyi1111@gmail.com>
2024-06-29 13:34:34 +0300

    api, data, antd, main stracture

---
+++ src/components/layout/AppSider.jsx
@@ -0,0 +1,18 @@
+import { Layout, Card, Space } from "antd";
+
+const siderStyle = {
+    textAlign: "center",
+    lineHeight: "120px",
+    color: "#fff",
+    backgroundColor: "#1677ff",
+};
+
+const AppSider = () => {
+    return (
+        <Layout.Sider width="25%" style={siderStyle}>
+            Sider
+        </Layout.Sider>
+    );
+};
+
+export default AppSider;
