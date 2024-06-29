a861f5e29eaaee17be20c75660a244d128de8bcb
Bogdan <bogdankreminskyi1507@gmail.com>
2024-06-29 20:59:00 +0300

    card added

--- src/components/layout/AppSider.jsx
+++ src/components/layout/AppSider.jsx
@@ -1,16 +1,35 @@
-import { Layout, Card, Space } from "antd";
+import { Layout, Card, Statistic } from "antd";
 
 const siderStyle = {
     textAlign: "center",
     lineHeight: "120px",
     color: "#fff",
     backgroundColor: "#1677ff",
+    padding: "1rem",
 };
 
 const AppSider = () => {
     return (
         <Layout.Sider width="25%" style={siderStyle}>
             Sider
+            <Card style={{ marginBottom: "1rem" }}>
+                <Statistic
+                    title="Active"
+                    value={11.28}
+                    precision={2}
+                    valueStyle={{ color: "#3f8600" }}
+                    suffix="%"
+                />
+            </Card>
+            <Card style={{ marginBottom: "1rem" }}>
+                <Statistic
+                    title="Active"
+                    value={11.28}
+                    precision={2}
+                    valueStyle={{ color: "#cf1322" }}
+                    suffix="%"
+                />
+            </Card>
         </Layout.Sider>
     );
 };
