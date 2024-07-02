5778cd9fabb0e9409089204b75da3559f850ff9f
Bogdan <bogdankreminskyi1507@gmail.com>
2024-07-02 12:04:39 +0300

    icons added, api

--- src/components/layout/AppSider.jsx
+++ src/components/layout/AppSider.jsx
@@ -1,4 +1,7 @@
-import { Layout, Card, Statistic } from "antd";
+import { useEffect, useState } from "react";
+import { fakeFetchCrypto, fakeFetchAssets } from "../../api";
+import { Layout, Card, Statistic, List, Typography, Spin } from "antd";
+import { ArrowUpOutlined, ArrowDownOutlined } from "@ant-design/icons";
 
 const siderStyle = {
     textAlign: "center",
@@ -8,7 +11,53 @@ const siderStyle = {
     padding: "1rem",
 };
 
+const data = [
+    "Racing car sprays burning fuel into crowd.",
+    "Japanese princess to wed commoner.",
+    "Australian walks 100km after outback crash.",
+    "Man charged over missing wedding girl.",
+    "Los Angeles battles huge wildfires.",
+];
+
+function percentDifference() {
+    return 100 * Math.abs((a - b) / ((a + b) / 2));
+}
+
 const AppSider = () => {
+    const [loading, setLoading] = useState(false);
+    const [crypto, setCrypto] = useState([]);
+    const [assets, setAssets] = useState([]);
+
+    useEffect(() => {
+        async function preload() {
+            setLoading(true);
+            const { result } = await fakeFetchCrypto();
+            const assets = await fakeFetchAssets();
+
+            setAssets(
+                assets.map((asset) => {
+                    const coin = result.find((c) => c.id === asset.id);
+                    return {
+                        grow: asset.price < coin.price,
+                        growPercent: percentDifference(asset.price, coin.price),
+                        totalAmount: asset.amount * coin.price,
+                        totalProfit:
+                            asset.amount * coin.price -
+                            asset.amount * asset.price,
+                        ...asset,
+                    };
+                })
+            );
+            setCrypto(result);
+            setLoading(false);
+        }
+        preload();
+    }, []);
+
+    if (loading) {
+        return <Spin fullscreen />;
+    }
+
     return (
         <Layout.Sider width="25%" style={siderStyle}>
             Sider
@@ -18,7 +67,18 @@ const AppSider = () => {
                     value={11.28}
                     precision={2}
                     valueStyle={{ color: "#3f8600" }}
+                    prefix={<ArrowUpOutlined />}
                     suffix="%"
+                />
+                <List
+                    size="small"
+                    dataSource={data}
+                    renderItem={(item) => (
+                        <List.Item>
+                            <Typography.Text mark>[ITEM]</Typography.Text>{" "}
+                            {item}
+                        </List.Item>
+                    )}
                 />
             </Card>
             <Card style={{ marginBottom: "1rem" }}>
@@ -27,6 +87,7 @@ const AppSider = () => {
                     value={11.28}
                     precision={2}
                     valueStyle={{ color: "#cf1322" }}
+                    prefix={<ArrowDownOutlined />}
                     suffix="%"
                 />
             </Card>
